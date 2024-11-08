8a40fdfe00377386b8f96a276b43834212501205
federicascognamiglio <scognamigliofede99@gmail.com>
2024-11-08 17:44:28 +0100

    feat: milestone 2 completed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 // DATI
 const ageInput = document.getElementById("age");
 const kmInput = document.getElementById("km");
+const nameInput = document.getElementById("username");
 const formElem = document.getElementById("ticket-form");
+const nameElem = document.getElementById("complete-name");
+const offerTypeElem = document.getElementById("offer-type");
+const carriageElem = document.getElementById("carriage");
+const cpCodeElem = document.getElementById("cp-code");
+const priceElem = document.getElementById("price");
 const pricePerKm = 0.21;
 const discountYoung = 20;
 const discountElderly = 40;
@@ -11,8 +17,14 @@ formElem.addEventListener("submit", function(event){
     event.preventDefault();
     const passengerAge = parseInt(ageInput.value.trim());
     const passengerKm = parseInt(kmInput.value.trim());
+    const passengerName = nameInput.value.trim();
     const finalPrice = ticketPrice(passengerAge, passengerKm, pricePerKm, discountYoung, discountElderly);
-    console.log(finalPrice);
+    nameElem.innerHTML = passengerName;
+    offerTypeElem.innerHTML = typeOfOffer(passengerAge);
+    carriageElem.innerHTML = randomNum(1, 10);
+    cpCodeElem.innerHTML = randomNum(1000, 3000);
+    priceElem.innerHTML = `${finalPrice} €`
+    formElem.reset();
 })
 
 // FUNZIONI
@@ -36,4 +48,32 @@ function ticketPrice (age, km, costKm, discount1, discount2) {
         ticketPrice -= discountCalc2;
     }
     return ticketPrice.toFixed(2);
+}
+
+/**
+ * Funzione che mi dice il tipo di offerta
+ * @param {number} age
+ * @returns {string}
+ */
+
+function typeOfOffer (age) {
+    let offerType = "Biglietto Standard";
+    if (age < 18) {
+        offerType = "Sconto Giovani";
+    } else if (age > 65) {
+        offerType = "Sconto Anziani";
+    }
+    return offerType;
+}
+
+
+// FUNZIONI
+/** Funzione che genera un numero random
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+*/
+
+function randomNum(min, max) {
+    return (Math.floor(Math.random() * (max - min) + min));
 }
